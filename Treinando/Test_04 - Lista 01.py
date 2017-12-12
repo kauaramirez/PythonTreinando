9dd2bedf88565bb94d8d0c5629db25ebfa8e73dd
kauaramirez <30813695+kauaramirez@users.noreply.github.com>
2017-12-12 13:48:37 -0200

    Create Test_04 - Lista 01.py

---
+++ Treinando/Test_04 - Lista 01.py
@@ -0,0 +1,23 @@
+# D. diff21
+# dado um inteiro n retorna a diferença absoluta entre n e 21
+# porém se o número for maior que 21 retorna dobro da diferença absoluta
+# diff21(19) -> 2
+# diff21(25) -> 8
+# dica: abs(x) retorna o valor absoluto de x
+
+def diff21(n):
+  dif = n - 21
+  if dif < 0:
+    dif = dif * -1
+  if n > 21:
+    dif = dif * 2
+  return dif
+
+def test_ex04():
+  print ('Diff21')
+  assert diff21(19) == 2
+  assert diff21(10) == 11
+  assert diff21(21) == 0
+  assert diff21(22) == 2
+  assert diff21(25) == 8
+  assert diff21(30) == 18
